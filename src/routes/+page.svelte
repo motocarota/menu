f468298c953822d7587987ca45e62240675f33cf
Motocarota <motocarota@gmail.com>
2024-06-22 15:46:59 +0200

    fix build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
 splash page
+<a href="/view/bagni-giovanni">bagni-giovanni</a>
+<a href="/view/delfino-bianco-special">delfino-bianco-special</a>
+<a href="/view/delfino-bianco-vini">delfino-bianco-vini</a>
