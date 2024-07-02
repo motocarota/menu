124036d9b4eba249ae696810db638136d49ab13e
Motocarota <motocarota@gmail.com>
2024-07-02 10:19:45 +0200

    fix print

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,5 @@
-<footer class="bg-white text-slate-800 p-2 text-right opacity-60 text-sm">
+<footer
+  class="bg-white text-slate-800 p-2 text-right opacity-60 text-sm no-print"
+>
   <a href="https://simone-poggi.com">made in Italy</a>
 </footer>
