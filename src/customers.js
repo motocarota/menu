9e90786db13b622c546638241ca40770e7ea28f9
Motocarota <motocarota@gmail.com>
2024-07-16 10:00:50 +0200

    mc

--- src/customers.js
+++ src/customers.js
@@ -1,9 +1,8 @@
 
+
+
 const customers = {
-	'delfino-bianco-vini': {
-		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv',
-		title: 'Ristorante Delfino Bianco',
-		subtitle: 'Menu dei vini',
+	'delfino-bianco': {
 		website: "www.ristorantedelfinobianco.it",
 		email: "ristorantedelfinobianco@gmail.com",
 		phone: ["+39.0185.450.837", "328.1467737"],
@@ -15,74 +14,98 @@ const customers = {
 		primaryColor: '#213f63',
 		secondaryColor: '#999999',
 		accentColor: '#296fae',
+		fontFamily: '"Josefin Sans", sans-serif',
+	},
+	'bagni-giovanni': {
+		address: "Via Lungomare Cavi di Lavagna (GE)",
+		website: "www.bagnigiovanni.it",
+		email: "bagnigiovanni@pec.it",
+		phone: ["+39 0185 696948"],
+		vat: "01002240990",
+
+		// theme
+		pic: 'https://bagnigiovanni.it/images/logo.webp',
+		bgColor: '#fdfdfd',
+		primaryColor: '#888',
+		secondaryColor: '#999999',
+		accentColor: '#296fae',
+		fontFamily: "Helvetica,Arial,sans-serif",
+	},
+	pescou: {
+		address: "Via Dante Alighieri, 70, 16033 Lavagna GE",
+		website: "",
+		email: "",
+		phone: ["+39 0185 392727"],
+		vat: "",
+
+		// theme
+		pic: undefined,
+		bgColor: '#fff',
+		primaryColor: '#333',
+		secondaryColor: '#3f3',
+		accentColor: '#f33',
+		fontFamily: 'Montserrat'
+	},
+	'desco-dei-fieschi': {
+		address: "Piazza della Libertà, 42, 16033 Lavagna GE",
+		website: "https://www.ildescodeifieschi.it/",
+		email: "",
+		phone: ["+39 0185 392765"],
+		vat: "02232360996",
+
+		// theme
+		pic: 'https://lirp.cdn-website.com/f70487f7/dms3rep/multi/opt/logo-il-desco-dei-fieschi-a3846b59-140h.png',
+		bgColor: '#cbe8f6',
+		primaryColor: '#19377c',
+		secondaryColor: '#333333',
+		accentColor: '#fff',
+		fontFamily: 'Poppins,"Poppins Fallback"'
+	}
+}
+
+const menus = {
+	'delfino-bianco-vini': {
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv',
+		title: 'Ristorante Delfino Bianco',
+		subtitle: 'Menu dei vini',
+		...customers['delfino-bianco'],
 	},
 	'delfino-bianco-special': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv',
 		title: "Ristorante Delfino Bianco",
 		subtitle: 'Menu special',
-		website: "www.ristorantedelfinobianco.it",
-		email: "ristorantedelfinobianco@gmail.com",
-		phone: ["+39.0185.450.837", "328.1467737"],
-		vat: "02391980998",
-
-		// theme
-		pic: 'https://ristorantedelfinobianco.it/img/menu.webp',
-		bgColor: '#fafafa',
-		primaryColor: '#213f63',
-		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		...customers['delfino-bianco'],
 	},
 	'bagni-giovanni': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=0&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu Ristorante',
-		address: "Via Lungomare Cavi di Lavagna (GE)",
-		website: "www.bagnigiovanni.it",
-		email: "bagnigiovanni@pec.it",
-		phone: ["+39 0185 696948"],
-		vat: "01002240990",
-
-		// theme
-		pic: 'https://bagnigiovanni.it/images/logo.png',
-		bgColor: '#fdfdfd',
-		primaryColor: '#213f63',
-		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		...customers['delfino-bianco'],
 	},
 	'bagni-giovanni-vini': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=2022557443&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu dei vini',
-		address: "Via Lungomare Cavi di Lavagna (GE)",
-		website: "www.bagnigiovanni.it",
-		email: "bagnigiovanni@pec.it",
-		phone: ["+39 0185 696948"],
-		vat: "01002240990",
-
-		// theme
-		pic: 'https://bagnigiovanni.it/images/logo.png',
-		bgColor: '#fdfdfd',
-		primaryColor: '#213f63',
-		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		...customers['bagni-giovanni'],
 	},
 	'bagni-giovanni-bar': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=1620509354&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu tavola fredda',
-		address: "Via Lungomare Cavi di Lavagna (GE)",
-		website: "www.bagnigiovanni.it",
-		email: "bagnigiovanni@pec.it",
-		phone: ["+39 0185 696948"],
-		vat: "01002240990",
-
-		// theme
-		pic: 'https://bagnigiovanni.it/images/logo.png',
-		bgColor: '#fdfdfd',
-		primaryColor: '#213f63',
-		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		...customers['bagni-giovanni'],
+	},
+	'pescou-vini': {
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRnaA-01E4XuwdiXk1gdW7wGIVqokg7IfEAMkn_-TxohUtr_LjVvaAiCTU5zwnEfe2Rv1gHNWH7ymXQ/pub?gid=2022557443&single=true&output=tsv',
+		title: 'Trattoria u Pescou',
+		subtitle: 'Ristorante di pesce',
+		...customers.pescou,
+	},
+	'fieschi-vini': {
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vThO3yYC0qBhvLAbrav7EUkzKOo2Q4WVHdGrPQ7MMJTqQRrPVLb5_ars-DFpHPpzEzyeREGCCFMyN1H/pub?gid=2022557443&single=true&output=tsv',
+		title: 'Il Desco dei Fieschi',
+		subtitle: 'Ristorante Pizzeria',
+		...customers['desco-dei-fieschi'],
 	},
 };
 
-export default customers
+export default menus
--- src/routes/view/[menuId]/+page.svelte
+++ src/routes/view/[menuId]/+page.svelte
@@ -1,10 +1,10 @@
 <script>
   import { onMount } from "svelte";
-  import MenuRow from '../../../lib/MenuWineRow.svelte'
-  import MenuFooter from '../../../lib/MenuFooter.svelte'
-  
+  import MenuRow from "../../../lib/MenuWineRow.svelte";
+  import MenuFooter from "../../../lib/MenuFooter.svelte";
+
   /** @type {import('./$types').PageData} */
-	export let data;
+  export let data;
 
   let posts = [];
   let loading = true;
@@ -13,17 +13,20 @@
     const endpoint = data?.url;
     const response = await fetch(endpoint);
     const res = await response.text();
-    
-		posts = (res.split('\r\n')).map(row => row.split('\t'));
+
+    posts = res.split("\r\n").map((row) => row.split("\t"));
     loading = false;
   });
-
 </script>
 
-<main style="background-color: {data.bgColor};">
+<main
+  style="background-color: {data.bgColor ??
+    '#efefef'}; font-family: {data.fontFamily}; color: {data.primaryColor ??
+    '#333'}"
+>
   <h1>{data.title}</h1>
   <h3>{data.subtitle}</h3>
-  <img src="{data.pic}" alt="" />
+  <img src={data.pic} alt="logo" />
   <table>
     {#if loading}
       <p>Caricamento...</p>
@@ -32,21 +35,12 @@
       <MenuRow {post} />
     {/each}
   </table>
-  <MenuFooter data={data} />
+  <MenuFooter {data} />
 </main>
 
 <style>
-  main {
-    text-align: center;
-  }
   img {
     margin: 3rem auto;
     max-height: 600px;
-  }
-  table {
-    width: 90%;
-    margin: 3rem auto;
-    border-collapse: separate;
-    border-spacing: 0 1em;
   }
 </style>
