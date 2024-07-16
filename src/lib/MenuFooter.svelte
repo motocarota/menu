fcd8c327bc2f56808a9142970ed8de75c106586d
Motocarota <motocarota@gmail.com>
2024-07-16 09:56:27 +0200

    links in footer

--- src/lib/MenuFooter.svelte
+++ src/lib/MenuFooter.svelte
@@ -1,19 +1,20 @@
 <script>
   export let data = {};
 </script>
+
 <footer class="bg-white text-slate-800 p pb-10">
   <div class="max-w-screen-lg mt-10 pt-10 mx-auto font-thin text-center">
-    <p>{data.website}</p>
+    <p><a href={data.website} target="_blank">{data.website}</a></p>
     <p>{data.address}</p>
     <p>
       {#each data.phone as p}
-        <a href="tel:{p}">{p}</a>
+        <a href="tel:{p}" target="_blank">{p}</a>
       {/each}
     </p>
     <p>
       <a href="mailto:?subject=informazioni" target="_blank">{data.email}</a>
     </p>
-    <br>
+    <br />
     <p>P.IVA/C.F. {data.vat}</p>
   </div>
 </footer>
