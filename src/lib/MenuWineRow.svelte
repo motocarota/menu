dcf5ecffb7bbf2b4e57af69eb0d0e49a085c1fb2
Motocarota <motocarota@gmail.com>
2024-07-27 15:30:38 +0200

    better font-style support

--- src/lib/MenuWineRow.svelte
+++ src/lib/MenuWineRow.svelte
@@ -1,23 +1,27 @@
 <script>
   export let post;
-  const [
-    color = "", name, notes, desc, price, qty = 0, 
-  ] = post ?? [];
+  export let titleColor = "#333";
+  export let altColor = "#333";
+  const [color = "", name, notes, desc, price, qty = 0] = post ?? [];
 
-  const n = notes ? `(${notes}) ` : '';
+  const n = notes ? `(${notes}) ` : "";
 </script>
 
 {#if qty && qty > 0}
-<tr>
-  {#if !price}
-    <th colspan="5" class="pt-5 text-xl font-extrabold">{name}</th>
-  {:else}
-    <td>
-      <div style="background: {color}" class='dot'></div>
-    </td>
-    <td><span class="text-xl">{name}</span></td>
-    <td>{n} {desc}</td>
-    <td class="price">{price}</td>
-  {/if}
+  <tr>
+    {#if !price}
+      <th
+        colspan="5"
+        style="color: {altColor}"
+        class="pt-5 text-xl font-extrabold">{name}</th
+      >
+    {:else}
+      <td>
+        <div style="background: {color}" class="dot"></div>
+      </td>
+      <td><span style="color: {titleColor}" class="text-xl">{name}</span></td>
+      <td>{n} {desc}</td>
+      <td class="price">{price}</td>
+    {/if}
   </tr>
 {/if}
