a257c547859db96dedd71fdcc212b647edd95298
Motocarota <motocarota@gmail.com>
2024-07-27 15:30:25 +0200

    mrf

--- src/menus.js
+++ src/menus.js
@@ -1,8 +1,6 @@
 
-
-
-const customers = {
-	'delfino-bianco': {
+const base = {
+	db: {
 		website: "https://www.ristorantedelfinobianco.it",
 		email: "ristorantedelfinobianco@gmail.com",
 		phone: ["+39.0185.450.837", "328.1467737"],
@@ -11,12 +9,12 @@ const customers = {
 		// theme
 		pic: 'https://ristorantedelfinobianco.it/img/wine.webp',
 		bgColor: '#fafafa',
-		primaryColor: '#213f63',
-		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		primaryColor: '#3c4073',
+		secondaryColor: '#91a0ad',
+		accentColor: '#3c4073',
 		fontFamily: '"Josefin Sans", sans-serif',
 	},
-	'bagni-giovanni': {
+	bg: {
 		address: "Via Lungomare Cavi di Lavagna (GE)",
 		website: "https://www.bagnigiovanni.it",
 		email: "bagnigiovanni@pec.it",
@@ -28,10 +26,10 @@ const customers = {
 		bgColor: '#fdfdfd',
 		primaryColor: '#888',
 		secondaryColor: '#999999',
-		accentColor: '#296fae',
+		accentColor: '#1f4258',
 		fontFamily: "Helvetica,Arial,sans-serif",
 	},
-	pescou: {
+	up: {
 		address: "Via Dante Alighieri, 70, 16033 Lavagna GE",
 		website: "",
 		email: "",
@@ -41,13 +39,13 @@ const customers = {
 		// theme
 		pic: undefined,
 		bgColor: '#fff',
-		primaryColor: '#333',
-		secondaryColor: '#3f3',
-		accentColor: '#f33',
-		fontFamily: 'Montserrat'
+		primaryColor: '#898989',
+		secondaryColor: '#3b5577',
+		accentColor: '#3b5577',
+		fontFamily: 'Red Hat Display,sans-serif'
 	},
-	'desco-dei-fieschi': {
-		address: "Piazza della Libertà, 42, 16033 Lavagna GE",
+	ddf: {
+		address: "Piazza della Libertà 42, 16033 Lavagna GE",
 		website: "https://www.ildescodeifieschi.it/",
 		email: "",
 		phone: ["+39 0185 392765"],
@@ -58,8 +56,23 @@ const customers = {
 		bgColor: '#cbe8f6',
 		primaryColor: '#19377c',
 		secondaryColor: '#333333',
-		accentColor: '#fff',
+		accentColor: '#19377c',
 		fontFamily: 'Poppins,"Poppins Fallback"'
+	},
+	mb: {
+		address: "Via Dante 56, 16033 Lavagna GE",
+		website: "https://www.monnabianca.it/",
+		email: "info@monnabianca.it",
+		phone: ["+39 0185 395452"],
+		vat: "01679860997",
+
+		// theme
+		pic: '',
+		bgColor: '#fff',
+		primaryColor: '#727272',
+		secondaryColor: '#0d0a47',
+		accentColor: '#a10d45',
+		fontFamily: 'Caudex,sans-serif'
 	}
 }
 
@@ -68,46 +81,52 @@ const menus = {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv',
 		title: 'Ristorante Delfino Bianco',
 		subtitle: 'Menu dei vini',
-		...customers['delfino-bianco'],
+		...base.db,
 	},
 	'delfino-bianco-special': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv',
 		title: "Ristorante Delfino Bianco",
 		subtitle: 'Menu special',
-		...customers['delfino-bianco'],
+		...base.db,
 	},
 
 	'bagni-giovanni': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=0&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu Ristorante',
-		...customers['bagni-giovanni'],
+		...base.bg,
 	},
 	'bagni-giovanni-vini': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=2022557443&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu dei vini',
-		...customers['bagni-giovanni'],
+		...base.bg,
 	},
 	'bagni-giovanni-bar': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8qhCOv9jDeoTc9afdaTMEvCh9yXXFjVRg855B9WSmr7jrgQipflrQ0cD3Q87bLQ5QvpsWaekF0Nyl/pub?gid=1620509354&single=true&output=tsv',
 		title: 'Bagni Giovanni',
 		subtitle: 'Menu tavola fredda',
-		...customers['bagni-giovanni'],
+		...base.bg,
 	},
 
 	'pescou-vini': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRnaA-01E4XuwdiXk1gdW7wGIVqokg7IfEAMkn_-TxohUtr_LjVvaAiCTU5zwnEfe2Rv1gHNWH7ymXQ/pub?gid=2022557443&single=true&output=tsv',
 		title: 'Trattoria u Pescou',
 		subtitle: 'Ristorante di pesce',
-		...customers.pescou,
+		...base.up,
 	},
 	
 	'fieschi-vini': {
 		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vThO3yYC0qBhvLAbrav7EUkzKOo2Q4WVHdGrPQ7MMJTqQRrPVLb5_ars-DFpHPpzEzyeREGCCFMyN1H/pub?gid=2022557443&single=true&output=tsv',
 		title: 'Il Desco dei Fieschi',
 		subtitle: 'Ristorante Pizzeria',
-		...customers['desco-dei-fieschi'],
+		...base.ddf,
+	},
+	'monna-bianca': {
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vThO3yYC0qBhvLAbrav7EUkzKOo2Q4WVHdGrPQ7MMJTqQRrPVLb5_ars-DFpHPpzEzyeREGCCFMyN1H/pub?gid=2022557443&single=true&output=tsv',
+		title: 'Monna Bianca',
+		subtitle: "Enoteca d'autore",
+		...base.mb,
 	},
 };
 
